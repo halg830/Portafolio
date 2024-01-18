d27d843f5d8b455744fa5bca0b059c394a0da3c2
halg830 <halg830@gmail.com>
2024-01-18 18:17:08 -0500

    animaciones

--- src/components/Portafolio.vue
+++ src/components/Portafolio.vue
@@ -53,63 +53,72 @@ function mostrarPDF() {
 
 <template>
   <main>
+    
     <section id="sectionPresentacion">
-      <img :src="foto" alt="Foto" id="foto"/>
+      <img :src="foto" alt="Foto" id="foto" />
       <article id="presentacion">
         <h1>Hola!!</h1>
         <h1>Soy Héctor Aguilar</h1>
         <h1>Desarrollador Full Stack</h1>
       </article>
     </section>
-    <section id="sectionSobreMi">
-      <article id="artSobreMi">
-        <h2>Sobre mí</h2>
-        <h4>¿Quién soy?</h4>
-        <p>
-          Soy un desarrollador de software apasionado y comprometido, dedicado a
-          enfrentar desafíos y encontrar soluciones efectivas. Mi enfoque se
-          centra en la resolución de problemas, utilizando habilidades
-          analíticas y creativas para superar obstáculos y lograr resultados
-          satisfactorios.
-        </p>
-        <q-btn @click="mostrarPDF">Descargar CV</q-btn>
-      </article>
-      <article id="artHabilidades">
-        <h2 style="padding: 30px;">Mis habilidades</h2>
-        <div id="habilidades">
-          <!-- Cuando pase el mouse por encima que el se muestre una barra de progreso -->
-          <div v-for="(h, i) in habilidades" :key="i" class="habilidad">
-            <img :src="h.img" alt="" />
-            <span>{{ h.nombre }}</span>
-          </div>
-        </div>
-      </article>
-    </section>
-    <section id="sectionProyectos">
-      <h2>Proyectos</h2>
-      <article id="secProyectos">
-        <div class="card" v-for="proyecto in proyectos" :key="proyecto.link">
-          <div class="icon">
-            <img :src="proyecto.img" alt="icon proyecto" style="height:40px; width:40px" />
-          </div>
-          <strong> {{ proyecto.nombre }}
-          </strong>
-          <div class="card__body">
-            {{ proyecto.descripcion }}
-          </div>
+    <div class="q-pa-md">
+      <div class="row justify-center q-gutter-sm">
+        <q-intersection transition="scale" transition-duration="1000" class="example-item">
+          <section id="sectionSobreMi">
+            <article id="artSobreMi">
+              <h2>Sobre mí</h2>
+              <h4>¿Quién soy?</h4>
+              <p>
+                Soy un desarrollador de software apasionado y comprometido, dedicado a
+                enfrentar desafíos y encontrar soluciones efectivas. Mi enfoque se
+                centra en la resolución de problemas, utilizando habilidades
+                analíticas y creativas para superar obstáculos y lograr resultados
+                satisfactorios.
+              </p>
+              <q-btn @click="mostrarPDF">Descargar CV</q-btn>
+            </article>
+            <article id="artHabilidades">
+              <h2 style="padding: 30px;">Mis habilidades</h2>
+              <div id="habilidades">
+                <!-- Cuando pase el mouse por encima que el se muestre una barra de progreso -->
+                <div v-for="(h, i) in habilidades" :key="i" class="habilidad">
+                  <img :src="h.img" alt="" />
+                  <span>{{ h.nombre }}</span>
+                </div>
+              </div>
+            </article>
+          </section>
+        </q-intersection>
+        <q-intersection transition="scale" transition-duration="1000" class="example-item">
+          <section id="sectionProyectos">
+            <h2>Proyectos</h2>
+            <article id="secProyectos">
+              <div class="card" v-for="proyecto in proyectos" :key="proyecto.link">
+                <div class="icon">
+                  <img :src="proyecto.img" alt="icon proyecto" style="height:40px; width:40px" />
+                </div>
+                <strong> {{ proyecto.nombre }}
+                </strong>
+                <div class="card__body">
+                  {{ proyecto.descripcion }}
+                </div>
 
-          <div class="btnOpciones">
-            <a :href="proyecto.link.deploy" class="links" target="_blank"><q-btn>Deploy</q-btn></a>
-            <a :href="proyecto.link.gitHub" v-if="proyecto.link.gitHub" class="links"
-              target="_blank"><q-btn>GitHub</q-btn></a>
-            <div v-else class="contLinks">
-              <a :href="proyecto.link.frontend" class="links" target="_blank"><q-btn>Frontend</q-btn></a>
-              <a :href="proyecto.link.backend" class="links" target="_blank"><q-btn>Backend</q-btn></a>
-            </div>
-          </div>
-        </div>
-      </article>
-    </section>
+                <div class="btnOpciones">
+                  <a :href="proyecto.link.deploy" class="links" target="_blank"><q-btn>Deploy</q-btn></a>
+                  <a :href="proyecto.link.gitHub" v-if="proyecto.link.gitHub" class="links"
+                    target="_blank"><q-btn>GitHub</q-btn></a>
+                  <div v-else class="contLinks">
+                    <a :href="proyecto.link.frontend" class="links" target="_blank"><q-btn>Frontend</q-btn></a>
+                    <a :href="proyecto.link.backend" class="links" target="_blank"><q-btn>Backend</q-btn></a>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </section>
+        </q-intersection>
+      </div>
+    </div>
     <footer>
       <a href="perfil.link" target="_blank" rel="noopener noreferrer" v-for="(perfil, i) in perfiles" :key="i">
         <img :src="perfil.img" />
@@ -128,7 +137,7 @@ function mostrarPDF() {
   /* box-shadow: 10px black; */
 }
 
-#foto{
+#foto {
   margin-bottom: 30px;
   border-radius: 100%;
   border: 3px solid black;
@@ -143,6 +152,7 @@ function mostrarPDF() {
 #sectionSobreMi {
   display: grid;
   grid-template-columns: 43% 57%;
+  margin-bottom: 40px;
 }
 
 #artSobreMi {
@@ -195,7 +205,7 @@ function mostrarPDF() {
 }
 
 #sectionProyectos {
-  padding: 40px;
+  padding: 0 40px;
   display: flex;
   flex-direction: column;
   align-items: center;
